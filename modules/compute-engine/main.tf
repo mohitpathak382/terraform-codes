42372cc1e99464bd00a93f030a1561794945e78a
Mohitpathak382 <Mohitpathakm382@gmail.com>
2025-07-23 16:52:41 +0530

    commpute code

---
+++ modules/compute-engine/main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "this" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnetwork
+    access_config {}  # remove this if you only want private IP; include if you need public
+  }
+
+  metadata_startup_script = var.metadata_startup_script
+
+  tags    = var.tags
+  labels  = var.labels
+}
---
+++ modules/compute-engine/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {}
+variable "zone" {}
+variable "machine_type" {}
+variable "image" {}
+variable "subnetwork" {}
+variable "metadata_startup_script" {}
+variable "tags" {
+  type = list(string)
+  default = []
+}
+variable "labels" {
+  type = map(string)
+  default = {}
+}
