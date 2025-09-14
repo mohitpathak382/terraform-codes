01a46627c0ec4ca22eb7d06e0836d591816953ca
Mohitpathak382 <Mohitpathakm382@gmail.com>
2025-09-14 20:50:43 +0530

    testing github actions workflows

--- Secrets/backend.tf
+++ Secrets/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "terraform-state-bkt2312"
-    prefix  = "terraform/state/secrets-manager"  # optional folder inside the bucket to organize states
+    prefix  = "terraform/state"  # optional folder inside the bucket to organize states
   }
 }
  
