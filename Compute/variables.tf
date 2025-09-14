3384ee4917a517a4378952a88acc9e37128079ef
Mohitpathak382 <Mohitpathakm382@gmail.com>
2025-09-15 01:25:05 +0530

    testing github actions workflows

--- Compute/variables.tf
+++ Compute/variables.tf
@@ -3,17 +3,17 @@ variable "region" {}
 variable "zone" {}
 variable "subnetwork" {}
 
-variable "image_family" {
-  default = "rhel-9"
-}
-variable "image_project" {
-  default = "rhel-cloud"
-}
+# variable "image_family" {
+#   default = "rhel-9"
+# }
+# variable "image_project" {
+#   default = "rhel-cloud"
+# }
 
 variable "frontend_config" {
   type = object({
     count             = number
-    machine_type      = string
+    # machine_type      = string
     startup_script    = string
     tags              = list(string)
     labels            = map(string)
