3cea8a4db65bcf17ef6227a7ca9cfd3acb9d9805
mohit pathak <mohitpathakm382@gmail.com>
2026-06-09 19:16:42 +0530

    test

--- Sql/main.tf
+++ Sql/main.tf
@@ -15,7 +15,8 @@ provider "google" {
 }
 
 module "cloudsql" {
-  source = "git::https://github.com/mohitpathak382/terraform-codes.git//modules/sql?ref=v1.0.2"
+  source = "git::https://github.com/mohitpathak382/terraform-codes.git//modules/sql?"
+  version = "v1.0.2"
 
   sql_config = var.sql_config
 }
