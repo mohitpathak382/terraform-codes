57e4c6e718826015bd86aed046e879d6f244596b
mohit pathak <mohitpathakm382@gmail.com>
2026-06-09 19:35:59 +0530

    test

--- Compute/backend.tf
+++ Compute/backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "gcs" {
-    bucket = "terraform-state-bkt2312"
-    prefix = "terraform/state/compute" # optional folder inside the bucket to organize states
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "terraform-state-bkt2312"
+#     prefix = "terraform/state/compute" # optional folder inside the bucket to organize states
+#   }
+# }
